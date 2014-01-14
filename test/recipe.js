d58b2cb241cf0c86396736a1feddafe2824a26cc
C.Jay Martin <cjay.martin@gmail.com>
2014-01-14 12:02:11 -0500

    Simple user test... also tidy recipe test (keep consistent!)

--- test/recipe.js
+++ test/recipe.js
@@ -1,92 +1,72 @@
-/*global describe:false, it:false, before:false, after:false, afterEach:false*/
-
-'use strict';
+"use strict";
 
 var nodeenv = process.env.NODE_ENV;
-if(! ( nodeenv && nodeenv === "test" ) ) {
-  process.env.NODE_ENV = "test";
-  console.log("SETTING ENV TO TEST");
+
+if (!(nodeenv && nodeenv === "test")) {
+    process.env.NODE_ENV = "test";
+    console.log("SETTING ENV TO TEST");
 }
 
+var app = require("../index"), mongoose = require("mongoose"), Recipe = require("../models/recipe"), kraken = require("kraken-js"), request = require("supertest"), assert = require("assert");
 
-var app = require('../index'),
-    mongoose = require('mongoose'),
-    Recipe = require('../models/recipe'),
-    kraken = require('kraken-js'),
-    request = require('supertest'),
-    assert = require('assert');
-
-
-describe('recipe', function () {
-
+describe("recipe", function() {
     var mock;
-
-
-    beforeEach(function (done) {
-        kraken.create(app).listen(function (err, server) {
-          mock = server;
-          done(err);
+    beforeEach(function(done) {
+        kraken.create(app).listen(function(err, server) {
+            mock = server;
+            done(err);
         });
     });
-
-
-    afterEach(function (done) {
-      Recipe.remove({},function() {
-        mongoose.connection.close();
-        mock.close(done);
-      });
+    afterEach(function(done) {
+        Recipe.remove({}, function() {
+            mongoose.connection.close();
+            mock.close(done);
+        });
     });
-
-
-    it('should say "hello"', function (done) {
-        request(mock)
-            .get('/')
-            .expect(200)
-            .expect('Content-Type', /html/)
-            .expect(/Hello, /)
-            .end(function(err, res){
-                if (err) return done(err);
-                done()
+    it('should say "hello"', function(done) {
+        request(mock).get("/").expect(200).expect("Content-Type", /html/).expect(/Hello, /).end(function(err, res) {
+            if (err) return done(err);
+            done();
+        });
+    });
+    it("should search recipes", function(done) {
+        request(mock).get("/").end(function(err, res) {
+            Recipe.find({}, function(e, o) {
+                var env = process.env.NODE_ENV;
+                assert(!e, "E is null");
+                done();
             });
-    });
-
-    it('should search recipes',function (done) {
-      request(mock).get('/').end(function(err,res) {     
-      Recipe.find({},function(e,o) {
-        var env = process.env.NODE_ENV;
-        
-        assert(!e,"E is null");        
-        done();
-      });
-    });
-    });
-
-    it('should add a recipe and find it',function (done) {
-      Recipe.create({
-        user_email: "test@user.com",
-        name: "Blah",
-        steps: ["Test","Then"],
-        ingredients: [
-           {raw_amount: '1/2',amount: 0.5, measure: 'tsp', ingredient: 'awesomeness'},
-           {raw_amount: '1/4',amount: 0.25, measure: 'tsp', ingredient: 'coolitude'},
-        ]
-      },function(e,o) {
-        assert(!e,"E is null");        
-        assert(o,"O is not null");        
-        Recipe.findOne({name: "Blah"},function(e,o2) {
-          assert(!e,"E is null");        
-          assert.equal(o.name,o2.name,"Got what I gave: name");
-          assert.equal(JSON.stringify(o.steps),JSON.stringify(o2.steps),"Got what I gave: steps");
-          assert.equal(o.description,o2.description,"Got what I gave: desc");
-          assert.equal(JSON.stringify(o.ingredients),JSON.stringify(o2.ingredients),"Got what I gave: ing");
-          done();
         });
-
-
-      });
-
-
-
     });
-
+    it("should add a recipe and find it", function(done) {
+        Recipe.create({
+            user_email: "test@user.com",
+            name: "Blah",
+            steps: [ "Test", "Then" ],
+            ingredients: [ {
+                raw_amount: "1/2",
+                amount: .5,
+                measure: "tsp",
+                ingredient: "awesomeness"
+            }, {
+                raw_amount: "1/4",
+                amount: .25,
+                measure: "tsp",
+                ingredient: "coolitude"
+            } ]
+        }, function(e, o) {
+            assert(!e, "E is null");
+            assert(o, "O is not null");
+            Recipe.findOne({
+                name: "Blah"
+            }, function(e, o2) {
+                assert(!e, "E is null");
+                assert.equal(o.name, o2.name, "Got what I gave: name");
+                assert.equal(JSON.stringify(o.steps), JSON.stringify(o2.steps), "Got what I gave: steps");
+                assert.equal(o.description, o2.description, "Got what I gave: desc");
+                assert.equal(JSON.stringify(o.ingredients), JSON.stringify(o2.ingredients), "Got what I gave: ing");
+                done();
+            });
+        });
+    });
 });
--- test/user.js
+++ test/user.js
@@ -1,49 +1,41 @@
-/*global describe:false, it:false, before:false, after:false, afterEach:false*/
-
-'use strict';
+"use strict";
 
 var nodeenv = process.env.NODE_ENV;
-if(! ( nodeenv && nodeenv === "test" ) ) {
-  process.env.NODE_ENV = "test";
-  console.log("SETTING ENV TO TEST");
+
+if (!(nodeenv && nodeenv === "test")) {
+    process.env.NODE_ENV = "test";
+    console.log("SETTING ENV TO TEST");
 }
 
-var app = require('../index'),
-    mongoose = require('mongoose'),
-    kraken = require('kraken-js'),
-    request = require('supertest'),
-    assert = require('assert');
+var app = require("../index"), mongoose = require("mongoose"), kraken = require("kraken-js"), request = require("supertest"), assert = require("assert");
 
-
-describe('user', function () {
-
+describe("user", function() {
     var mock;
-
-
-    beforeEach(function (done) {
-        kraken.create(app).listen(function (err, server) {
+    beforeEach(function(done) {
+        kraken.create(app).listen(function(err, server) {
             mock = server;
             done(err);
         });
     });
-
-
-    afterEach(function (done) {
-        mongoose.connection.close();
-        mock.close(done);
+    afterEach(function(done) {
+        User.remove({}, function() {
+            mongoose.connection.close();
+            mock.close(done);
+        });
     });
-
-
-    it('should say "hello"', function (done) {
-        request(mock)
-            .get('/')
-            .expect(200)
-            .expect('Content-Type', /html/)
-            .expect(/Hello, /)
-            .end(function(err, res){
-                if (err) return done(err);
-                done()
+    it('should say "hello"', function(done) {
+        request(mock).get("/").expect(200).expect("Content-Type", /html/).expect(/Hello, /).end(function(err, res) {
+            if (err) return done(err);
+            done();
+        });
+    });
+    it("should search users", function(done) {
+        request(mock).get("/").end(function(err, res) {
+            User.find({}, function(e, o) {
+                var env = process.env.NODE_ENV;
+                assert(!e, "E is null");
+                done();
             });
+        });
     });
-
 });
